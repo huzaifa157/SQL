ff189f032c95fcbc695d34fb0fb3efdc7fe5e7fd
huzaifa157 <m.huzaifa157@gmail.com>
2025-09-03 20:03:50 +0500

    using faker

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 // index.js
 import { faker } from '@faker-js/faker';
 
-let createRandomUser = () => {
+let getRandomUser = () => {
   return {
-    userId: faker.string.uuid(),
+    id: faker.string.uuid(),
     username: faker.internet.username(),
     email: faker.internet.email(),
-    avatar: faker.image.avatar(),
     password: faker.internet.password(),
-    birthdate: faker.date.birthdate(),
-    registeredAt: faker.date.past(),
+   
   };
 };
 
-console.log(createRandomUser());
+console.log(getRandomUser());
