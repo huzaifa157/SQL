8e85e4a88bf03c2c13a2b561ab3cc40543b2a60c
huzaifa157 <m.huzaifa157@gmail.com>
2025-09-03 19:49:44 +0500

    install faker

---
+++ index.js
@@ -0,0 +1,16 @@
+// index.js
+import { faker } from '@faker-js/faker';
+
+let createRandomUser = () => {
+  return {
+    userId: faker.string.uuid(),
+    username: faker.internet.username(),
+    email: faker.internet.email(),
+    avatar: faker.image.avatar(),
+    password: faker.internet.password(),
+    birthdate: faker.date.birthdate(),
+    registeredAt: faker.date.past(),
+  };
+};
+
+console.log(createRandomUser());
