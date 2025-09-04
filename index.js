53d90b7d39975e054e74ca15fc61e6642c706bc4
huzaifa157 <m.huzaifa157@gmail.com>
2025-09-04 06:15:31 +0500

    creating user table

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
 // index.js
 import { faker } from '@faker-js/faker';
+import mysql from 'mysql2';
+
+// Create the connection to database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'delta',
+  password: 'google6464'
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error("Error connecting to MySQL:", err);
+    return;
+  }
+  console.log("Connected to MySQL!");
+
+  // Run query with callback
+  connection.query("SHOW TABLES", (err, results) => {
+    if (err) {
+      console.error("Error executing query:", err);
+      return;
+    }
+    console.log("Tables:", results);
+    connection.end();
+  });
+});
+
+
+
 
 let getRandomUser = () => {
   return {
@@ -7,8 +37,5 @@ let getRandomUser = () => {
     username: faker.internet.username(),
     email: faker.internet.email(),
     password: faker.internet.password(),
-   
   };
 };
-
-console.log(getRandomUser());
---
+++ scheme.sql
@@ -0,0 +1,8 @@
+CREATE TABLE user(
+    id INT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR(40) NOT NULL UNIQUE,
+    password VARCHAR(50) NOT NULL UNIQUE
+
+
+);
