decabfdea1a82922accf2268870006f7d72b999e
huzaifa157 <m.huzaifa157@gmail.com>
2025-09-05 12:03:11 +0500

    inseat data into table

--- index.js
+++ index.js
@@ -4,38 +4,32 @@ import mysql from 'mysql2';
 
 // Create the connection to database
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  database: 'delta',
-  password: 'google6464'
+    host: 'localhost',
+    user: 'root',
+    database: 'delta',
+    password: 'google6464'
 });
+  
+let q = "INSERT INTO user (id , username , email, password) VALUES (?,?,?,?)";
+let user= ["586", "shayan_cheema" , "cheema@yayhoo.com" ,"pass123" ]
 
-connection.connect((err) => {
-  if (err) {
-    console.error("Error connecting to MySQL:", err);
-    return;
-  }
-  console.log("Connected to MySQL!");
+try{
+    connection.query(q, user, (err,result)=>{
+        if(err) throw err;
+        console.log(result);
+    });    
+}catch(err){
+    console.log(err);
+}
 
-  // Run query with callback
-  connection.query("SHOW TABLES", (err, results) => {
-    if (err) {
-      console.error("Error executing query:", err);
-      return;
-    }
-    console.log("Tables:", results);
-    connection.end();
-  });
-});
-
-
+connection.end();
 
 
 let getRandomUser = () => {
-  return {
-    id: faker.string.uuid(),
-    username: faker.internet.username(),
-    email: faker.internet.email(),
-    password: faker.internet.password(),
-  };
+    return {
+        id: faker.string.uuid(),
+        username: faker.internet.username(),
+        email: faker.internet.email(),
+        password: faker.internet.password(),
+    };
 };
